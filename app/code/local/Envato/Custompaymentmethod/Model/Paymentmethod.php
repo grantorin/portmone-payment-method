337ebaeb47b3aeb773917b608f9d8b69011c77f7
Gran Torino <red.chekist@gmail.com>
2018-06-14 10:45:16 +0300

    base

--- app/code/local/Envato/Custompaymentmethod/Model/Paymentmethod.php
+++ app/code/local/Envato/Custompaymentmethod/Model/Paymentmethod.php
@@ -21,34 +21,10 @@ class Envato_Custompaymentmethod_Model_Paymentmethod extends Mage_Payment_Model_
 
         return $this;
     }
-
-    public function validate()
-    {
-        parent::validate();
-        $info = $this->getInfoInstance();
-
-        if (!$info->getCustomFieldOne())
-        {
-            $errorCode = 'invalid_data';
-            $errorMsg = $this->_getHelper()->__("CustomFieldOne is a required field.\n");
-        }
-
-        if (!$info->getCustomFieldTwo())
-        {
-            $errorCode = 'invalid_data';
-            $errorMsg .= $this->_getHelper()->__('CustomFieldTwo is a required field.');
-        }
-
-        if ($errorMsg)
-        {
-            Mage::throwException($errorMsg);
-        }
-
-        return $this;
-    }
-
     public function getOrderPlaceRedirectUrl()
     {
-        return Mage::getUrl('custompaymentmethod/payment/redirect', array('_secure' => false));
+//        return 'https//www.portmone.com.ua/gateway/';
+        $this->_redirect('checkout/onepage/success', array('_secure'=>true));
+        return false;
     }
 }
